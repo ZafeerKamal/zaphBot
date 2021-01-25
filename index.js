ed61c26e5f145d022fcf8d7ac8f19adb9bf0c411
ZafeerKamal <zafeer.kamal@gmail.com>
2021-01-25 12:36:37 -0500

    Restructured assets

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ client.on('message', (message) => {
 
     //if (message.content === 'among us' ) {
     if (message.content.includes("among us")) {
-        message.channel.send({files: ["./imp.jpg"]});
+        message.channel.send({files: ["./Assets/imp.jpg"]});
         const { voice } = message.member
         if (!voice.channelID) {
             message.reply('You must be in a voice channel')
             return
         }
         voice.channel.join().then((connection) => {
-            connection.play(path.join(__dirname, 'AU.mp3'))
+            connection.play(path.join(__dirname, 'Assets/AU.mp3'))
         })
     }
 
@@ -46,22 +46,22 @@ client.on('message', (message) => {
 
         switch(x) {
             case 0:
-                message.channel.send("no valorant allowed", {files: ["./val.png"]}) 
+                message.channel.send("no valorant allowed", {files: ["./Assets/val.png"]}) 
                 break;
             case 1: 
-                message.channel.send("no valorant allowed", {files: ["./val2.png"]}) 
+                message.channel.send("no valorant allowed", {files: ["./Assets/val2.png"]}) 
                 break;
             case 2: 
-                message.channel.send("no valorant allowed", {files: ["./val3.png"]}) 
+                message.channel.send("no valorant allowed", {files: ["./Assets/val3.png"]}) 
                 break;
             case 3: 
-                message.channel.send("no valorant allowed", {files: ["./val4.png"]}) 
+                message.channel.send("no valorant allowed", {files: ["./Assets/val4.png"]}) 
                 break;
             case 4: 
-                message.channel.send("no valorant allowed", {files: ["./val5.png"]}) 
+                message.channel.send("no valorant allowed", {files: ["./Assets/val5.png"]}) 
                 break;
             case 5: 
-                message.channel.send("no valorant allowed", {files: ["./val6.png"]}) 
+                message.channel.send("no valorant allowed", {files: ["./Assets/val6.png"]}) 
                 break;
         }
 
